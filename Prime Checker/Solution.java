30b9791275a94e27f72290a48728db20a87cf8ce
Chinmay Bhardwaj <chinmay.bhardwaj@gmail.com>
2018-07-31 15:38:16 +0530

    Create Solution.java

---
+++ Prime Checker/Solution.java
@@ -0,0 +1,65 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+import static java.lang.System.in;
+
+class Prime {
+
+    public void checkPrime(Integer... b) {
+        for (int num : b) {
+            System.out.print(isPrime(num) ? "" + num + " " : "");
+        }
+        System.out.println("");
+    }
+
+    //checks whether an int is prime or not.
+    public boolean isPrime(int num) {
+        if (num == 0) return false;
+        if (num == 1) return false;
+        for (int i = 2; i < num / 2 + 1; i++) {
+            if ((num % i) == 0) return false;
+        }
+
+        return true;
+    }
+}
+
+public class Solution {
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            int n1 = Integer.parseInt(br.readLine());
+            int n2 = Integer.parseInt(br.readLine());
+            int n3 = Integer.parseInt(br.readLine());
+            int n4 = Integer.parseInt(br.readLine());
+            int n5 = Integer.parseInt(br.readLine());
+            Prime ob = new Prime();
+            ob.checkPrime(n1);
+            ob.checkPrime(n1, n2);
+            ob.checkPrime(n1, n2, n3);
+            ob.checkPrime(n1, n2, n3, n4, n5);
+            Method[] methods = Prime.class.getDeclaredMethods();
+            Set<String> set = new HashSet<>();
+            boolean overload = false;
+            for (int i = 0; i < methods.length; i++) {
+                if (set.contains(methods[i].getName())) {
+                    overload = true;
+                    break;
+                }
+                set.add(methods[i].getName());
+
+            }
+            if (overload) {
+                throw new Exception("Overloading not allowed");
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
